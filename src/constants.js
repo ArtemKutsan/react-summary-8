b32c7e2431f48667799df9be755ad6b69c8e70d5
Artem Kutsan <artemkutsan@gmail.com>
2026-03-22 13:08:49 +0100

    refactor: вынесение массивов констант в constants.js для общего использования в компонентах ProfileCard и ProfileControls

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const professions = {
+  developer: 'Разработчик',
+  designer: 'Дизайнер',
+  manager: 'Менеджер',
+  analyst: 'Аналитик',
+};
+
+export const jobButtonTexts = {
+  developer: 'Предложить проект',
+  designer: 'Предложить заказ',
+  manager: 'Предложить вакансию',
+  analyst: 'Предложить стажировку',
+};
